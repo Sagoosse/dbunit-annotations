3b6dc1accd018bda1b9e90be3038fe28f9ea92de
SARAH <sarahgoossens1704@gmail.com>
2017-03-28 14:54:27 +0200

    modif

--- src/main/java/org/dbunit/ext/annotation/factory/AbstractDataSetFactory.java
+++ src/main/java/org/dbunit/ext/annotation/factory/AbstractDataSetFactory.java
@@ -27,7 +27,7 @@ public abstract class AbstractDataSetFactory<A extends Annotation> implements ID
 	public final IDataSet create(A annotation, Method method, Integer parameterPosition, Object target) throws Exception {
 		IDataSet result = null ;
 		IDataSet[] dataSets = createDataSets(annotation, method, parameterPosition, target);
-		if (dataSets.length == 0) {
+		if (dataSets.length > 0) {
 			result = new DefaultDataSet() ;
 		}
 		else if (dataSets.length == 1) {
